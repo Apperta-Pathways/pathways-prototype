720d03ac8ecea5da4b1fa79f1e972a52b63f3ca5
Alexander Gamble <alexander.gamble@outlook.com>
2015-02-04 12:56:46 +0000

    added proper relationships between patients, teams and doctors, added factories for 2 different types of team

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,7 @@
 class Doctor < ActiveRecord::Base
+  has_and_belongs_to_many :teams
+  has_and_belongs_to_many :patients, through: :teams
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
