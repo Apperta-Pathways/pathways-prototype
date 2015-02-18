6c9d80bcf4329a8fefcf69bb8216723fe62f9ca5
iustin <iustin@yahoo.com>
2015-02-18 23:38:27 +0000

    Team members no doubles allowed

--- app/controllers/team_controller.rb
+++ app/controllers/team_controller.rb
@@ -1,12 +1,16 @@
 class TeamController < ApplicationController
   
   def add
-    if(params[:typeAdded] == 'Patient' && !Patient.find_by(email: params[:team_member][:email]).nil?)
-      Team.find_by(id:params[:id]).patients << [Patient.find_by(email: params[:team_member][:email])]
+    @team = Team.find_by(id:params[:id])
+    @patient = Patient.find_by(email: params[:team_member][:email])
+    @doctor = Doctor.find_by(email: params[:team_member][:email])
+    
+    if(params[:typeAdded] == 'Patient' && !@patient.nil? && !@team.patients.include?(@patient))
+      @team.patients << [@patient]
     end
     
-    if(params[:typeAdded] == 'Doctor' && !Doctor.find_by(email: params[:team_member][:email]).nil?)
-      Team.find_by(id:params[:id]).doctors << [Doctor.find_by(email: params[:team_member][:email])]
+    if(params[:typeAdded] == 'Doctor' && !@doctor.nil? && !@team.doctors.include?(@doctor))
+      @team.doctors << [@doctor]
     end
     
     redirect_to Team.find_by(id: params[:id])
