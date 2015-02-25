cf04d1f723f6c2e59a3fd584a53279203494f7e1
Alexander Gamble <alexander.gamble@outlook.com>
2015-02-25 16:36:14 +0000

    added rvm version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,6 @@
 # config valid only for current version of Capistrano
+require "rvm/capistrano"
+
 lock '3.3.5'
 
 set :application, 'gstt-rails'
@@ -10,35 +12,10 @@ set :pty, true
 set :format, :pretty
 
 set :default_env, { rvm_bin_path: '~/.rvm/bin' }
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :rvm_ruby_string, :local
 
-# Default value for :scm is :git
-# set :scm, :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 
 namespace :deploy do
 
