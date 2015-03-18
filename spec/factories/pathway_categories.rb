8303693efbafe6973b6fea26a1fa8b4f2bec950b
Alexander Gamble <alexander.gamble@outlook.com>
2015-03-18 12:25:08 +0000

    added travis

--- spec/factories/pathway_categories.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :pathway_category do
-    pathway_id 1
-    category_id 1
-  end
-end
--- spec/factories/treatment_module_subcategories.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :treatment_module_subcategory do
-    treatment_state_id 1
-    subcategory_id 1
-  end
-end
