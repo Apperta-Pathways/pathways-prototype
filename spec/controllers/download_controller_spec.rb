97b3043f7d09ce005eb2b7d9190bf4d06cd88364
Alexander Gamble <alexander.gamble@outlook.com>
2015-03-30 10:36:06 +0100

    removed another test

--- spec/controllers/download_controller_spec.rb
+++ spec/controllers/download_controller_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 RSpec.describe DownloadController, :type => :controller do
 
   describe "GET log" do
-    it "returns http success" do
-      get :log
-      expect(response).to be_success
-    end
   end
 
 end
