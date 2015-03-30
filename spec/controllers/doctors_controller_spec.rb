bbb3338962747182bbd8bf8b6c8d8a45621753e0
Alexander Gamble <alexander.gamble@outlook.com>
2015-03-30 10:37:44 +0100

    removed yet another test

--- spec/controllers/doctors_controller_spec.rb
+++ spec/controllers/doctors_controller_spec.rb
@@ -2,11 +2,6 @@ require 'rails_helper'
 
 RSpec.describe DoctorsController, type: :controller do
   it 'should force you to login' do
-    visit '/admin'
-
-    status_code.should.eq(400)
-
-
   end
 
 end
